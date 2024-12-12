799fca1a836afc47d22c858ad73398bc47e5321e
matthew <matvey.karasev.00@gmail.com>
2024-12-12 15:10:46 +0300

    HW 15.1 was completed

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -5,5 +5,11 @@ public class Main {
     // Коды даны для примера и не соответствуют действительности. Учитывайте, что нужно распечатать коды букв в двух регистрах!
     public static void main(String[] args) {
         //todo код писать тут
+        for (int i = 'А'; i <='Я'; i++) {
+            System.out.print((char) (i) + "-" + i + " ");
+            System.out.println((char) (i + 32) + "-" + (i + 32));
+        }
+
+
     }
 }
